6bb3643317d25f8f16a6e62e8aff85c318063624
natalienickells <nanick73@bas.ac.uk>
2023-08-30 11:21:20 +0100

    Added column names to net monitor read in, from tpl file.

--- Creating EVL files 300823.R
+++ Creating EVL files 300823.R
@@ -13,11 +13,18 @@ setwd("D:/Cruise_data/")
 library(tidyverse)
 
 #Reading in ACO files
-
 #IMPORTANT: Before reading in ACO files, need to open them in Notepad and save them as txt. 
-#If they are read in as aco, using the 'swatches' package, a vector of 00s is generated. 
+#(Naming convention= "netmonitor.txt")
+#If they are read in as aco, using the 'swatches' package, a vector of 00s is generated. (error!)
 
-
-
-
+nm <- read.table("JR15002/netmonitor.txt", sep=",")
+#17 columns, currently with no headers. Need to add these headers.
   
+tplcolnames <- read.table("JR15002/netmonitor.tpl.colnames.txt")
+tplcolnames<- unlist(as.vector(tplcolnames))
+
+
+colnames(nm)<- c("Year", "J.Date", "J.Day", "J.Time", tplcolnames)
+
+
+
