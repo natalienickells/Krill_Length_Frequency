118350282284adefff71a29d009b7421968e1a0d
natalienickells <nanick73@bas.ac.uk>
2023-08-30 10:26:20 +0100

    Initial commit.

---
+++ Creating EVL files 300823.R
@@ -0,0 +1,16 @@
+#Date: 30th August 2023
+#Author:Natalie Nickells
+
+#Aim: To create EVL files from net data in ACO and TPL formats. 
+#These EVL files will show the 'line of travel' of the net and can then be read into Echoview. 
+
+
+#PREPARATION==============================================================================
+
+#Loading required packages
+library(tidyverse)
+library(swatches) #need this package to read in the Aco file
+
+#Reading in ACO files
+
+read_aco
