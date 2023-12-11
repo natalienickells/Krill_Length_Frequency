39294977bfb1f0b428fea60f77c9624720f5583b
natalienickells <nanick73@bas.ac.uk>
2023-12-11 16:44:00 +0000

    Initial commit

---
+++ 1 Listing nets and cruises where krill were measured.R
@@ -0,0 +1,16 @@
+ 
+#Natalie Nickells
+#11th December 2023
+#Aim: Identifying cruises & nets for which I have krill length data
+
+#This script will be written in R Studio but version control will be used, 
+#in GitHub repo Krill_Length_Frequency
+
+#This script is an updated version of "ExtractingEventNAmes.R", at filepath D:\R code\ExtractingEventNAmes.R
+
+#SET UP=========================================================================
+#Loading required packages
+library(tidyverse)
+library(readxl)
+library(openxlsx)
+
