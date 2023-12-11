fee9eb894f4db370361ecb1da7c1161f6bc5ddf6
natalienickells <nanick73@bas.ac.uk>
2023-12-11 17:54:13 +0000

    End of day commit

--- 1 Listing nets and cruises where krill were measured.R
+++ 1 Listing nets and cruises where krill were measured.R
@@ -11,6 +11,26 @@
 #SET UP=========================================================================
 #Loading required packages
 library(tidyverse)
-library(readxl)
+
 library(openxlsx)
+
+#Setting working directory 
+setwd("D:\\Cruise_data\\")
+
+#DATA IMPORT====================================================================
+#My data for each cruise has a different name and filepath. 
+#I have a spreadsheet where these different filepaths are kept, so will first read this in. 
+
+library(readxl)
+filepathinfo<- read_excel("D:\\KLF Cruises & Nets - Data Tracking Sheet.xlsx")
+
+#need columns 1 to 20, and columns Cruise_ID, KL_Data_Format and KL_Data_Filepath
+filepathinfo<- select(filepathinfo, Cruise_ID, KL_Data_Filepath, KL_Data_Format) 
+filepathinfo<- slice(filepathinfo, 1:20)
+
+
+print(filepathinfo$KL_Data_Filepath)
+
+
+
 
