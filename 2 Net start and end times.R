8416f8ebf7b9506f99141b39d76652face3cabbc
natalienickells <nanick73@bas.ac.uk>
2023-12-12 23:22:31 +0000

    initial commit

---
+++ 2 Net start and end times.R
@@ -0,0 +1,13 @@
+# 2 Getting net start and end times
+
+
+#Natalie Nickells
+#12th December 2023
+
+#Aim: to get net start and end times from a list of nets where krill were measured. 
+
+
+library(usethis)
+use_git()
+
+C:/Users/user/Desktop/Krill_Length_Frequency_laptop/1 Listing nets and cruises where krill were measured.R
