caf4fca652ab8ed21c1a89598f271cf1d462f49a
natalienickells <nanick73@bas.ac.uk>
2024-01-02 17:36:00 +0000

    Initial commit

---
+++ 4 Creating net EVRs and EVLs.R
@@ -0,0 +1,23 @@
+#4 Getting net EVRs and EVLs (from depth data)
+
+#Natalie Nickells 
+#2nd Jan 2024
+
+#Aim: To create EVLs (EVRs?) for each net, using net start/end times and depth data. 
+
+#This script will be written in R Studio but version control will be used, 
+#in GitHub repo Krill_Length_Frequency
+#This script is an updated version/draws heavily on XX, 
+#at filepath XX
+
+#Data sources:
+
+#Data outputs:
+
+#Current known errors: 
+
+
+
+
+
+
