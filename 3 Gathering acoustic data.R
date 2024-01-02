ddc730dcb3080507022a73458faa060ee3dbf352
natalienickells <nanick73@bas.ac.uk>
2024-01-02 15:22:55 +0000

    initial commit

---
+++ 3 Gathering acoustic data.R
@@ -0,0 +1,20 @@
+# 3 Gathering acoustic files needed
+
+#Natalie Nickells
+#2nd January 2024
+
+#Aim: to gather the acoustic files I will need to make my EV files together into one folder.
+#These will be from the net times where krill were measured, from various cruises. 
+
+
+#This script will be written in R Studio but version control will be used, 
+#in GitHub repo Krill_Length_Frequency
+
+#This script is an updated version/draws heavily on "Selecting_raw_files040823.R", 
+#at filepath "D:\R code\Selecting_raw_files040823.R"
+
+
+#Since acoustic files use a huge amount of storage, and each cruise has a large
+#amount of acoustic data, I will gather together only the acoustic data around 
+#the nets where krill have been measured. 
+
